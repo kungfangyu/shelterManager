0363154428973474649f25faf9a06d1b56433a37
Fane <fish19921026@gmail.com>
2024-02-21 14:59:35 +0000

    remove: remove unused class.

--- src/uk/ac/ncl/csc/coursework/test/CustomerRecordTest.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.ncl.csc.coursework.test;
-
-public class CustomerRecordTest {
-    public static void main(String[] args) {
-        CustomerRecordTest customerRecordTest = new CustomerRecordTest();
-        System.out.println("Test customer record:");
-    }
-}
